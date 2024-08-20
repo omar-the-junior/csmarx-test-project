63e4f21b00d301e97c17542a460d90d143824d49
omar-the-junior <omarmasoudonline@gmail.com>
2024-08-20 14:55:09 +0300

    Full landing page implementaion

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Initial State of the test project
+# C-Smarx Landing page
+
+This is the landing page for the C-Smarx project. It was created using HTML, tailwindcss, and daisyUI.
+
+# Go to [Landing page preview](https://csmarx-test-project.vercel.app/)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
+const { addIconSelectors } = require("@iconify/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#30aedb",
+
+          secondary: "#3076aa",
+
+          accent: "#11044f",
+
+          neutral: "#f4f7fd",
+
+          "base-100": "#ffffff",
+
+          info: "#22d3ee",
+
+          success: "#34d399",
+
+          warning: "#facc15",
+
+          error: "#ff0000",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui"), addDynamicIconSelectors()],
 };
